c4c074f2a00cef2b1985c62e36202abe052f9c10
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-29 16:28:34 +0100

    feat: pop

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -11,8 +11,8 @@ import java.util.Comparator;
  */
 class BinaryHeap<T> {
 
-  private ArrayList<T> heap;
-  private Comparator<T> comparator;
+  protected ArrayList<T> heap;
+  private final Comparator<T> comparator;
 
   public BinaryHeap(Comparator<T> comparator) {
     this.comparator = comparator;
@@ -20,18 +20,47 @@ class BinaryHeap<T> {
   }
 
   /**
-   * pop returns and removes the minimum object in the heap. If the heap is empty it throws a NotSuchElementException
+   * returns and removes the minimum object in the heap. If the heap is empty it throws a NotSuchElementException
    *
-   * @return
+   * @return the minimum object in the heap
    */
   public T pop() {
-    return null;
+    if (this.count() == 0) {
+      throw new java.util.NoSuchElementException();
+    }
+    // Replace the root of the heap with the last element on the last level.
+    T min = this.heap.get(0);
+    this.heap.set(0, this.heap.get(this.count() - 1));
+    this.heap.remove(this.count() - 1);
+    // Compare the new root with its children; if they are in the correct order, stop.
+    // Otherwise, swap the root with the smallest child and repeat the process.
+    int i = 0;
+    while (i < this.count()) {
+      int left = 2 * i + 1;
+      int right = 2 * i + 2;
+      int smallest = i;
+      if (left < this.count() && this.comparator.compare(this.heap.get(left), this.heap.get(smallest)) < 0) {
+        smallest = left;
+      }
+      if (right < this.count() && this.comparator.compare(this.heap.get(right), this.heap.get(smallest)) < 0) {
+        smallest = right;
+      }
+      if (smallest != i) {
+        T tmp = this.heap.get(i);
+        this.heap.set(i, this.heap.get(smallest));
+        this.heap.set(smallest, tmp);
+        i = smallest;
+      } else {
+        break;
+      }
+    }
+    return min;
   }
 
   /**
    * peek similar to pop, returns the minimum object but it does not remove it from the BinaryHeap
    *
-   * @return
+   * @return the minimum object
    */
   public T peek() {
     return heap.get(0);
@@ -61,12 +90,12 @@ class BinaryHeap<T> {
   }
 
   /**
-   * count returns the number of elements in the BinaryHeap
+   * returns the number of elements in the BinaryHeap
    *
-   * @return
+   * @return the number of elements in the BinaryHeap
    */
   public int count() {
-    return 0;
+    return heap.size();
   }
 
 }
